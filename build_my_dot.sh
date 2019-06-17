4e1b11041da73b979c4f067fa9c3247072f71f2a
Gavin Chou <gavineaglechou@gmail.com>
2019-06-17 18:56:12 +0800

    First commit

---
+++ build_my_dot.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+## @brief  build dot file to svg with js plugin embedded
+## @author zhoufei
+## @email  zhoufei05@baidu.com
+## @date   2019-06-17-Mon
+
+cwd=`pwd`
+
+# check if symlink
+file=`readlink $0`
+if [ "x${file}" == "x" ]; then
+  file=$0
+fi
+pushd "$(dirname ${file})" > /dev/null
+attach=`pwd -P`/plugin.xml
+popd > /dev/null
+
+src=$1
+out=$2
+
+if [ ! -f ${attach} ]; then
+  echo "attachment: ${attach} not found, unable to continue"
+  exit -1
+fi
+
+if [ "x${src}" == "x" ]; then
+  echo "usage:"
+  echo "  $0 "'${src} ${out}'
+fi
+
+if [ "x${out}" == "x" ]; then
+  out=${cwd}/a.svg
+else
+  if [[ "${out}" != "/*" ]] || [[ "${out}" != "~*" ]]; then
+    out=${out}
+  else
+    out="${cwd}/${out}"
+  fi
+fi
+
+# echo ${out}
+
+dot -Tsvg -o ${out} ${src} > /dev/null
+sed -ri 's#</svg>##' ${out}
+cat "${attach}" >> ${out}
+echo "</svg>" >> ${out}
